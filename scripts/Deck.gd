c706162dc2b9a4e835951c816fd4e4c566222390
LDRentil <pylev2000@gmail.com>
2021-06-25 14:24:32 +0300

    Method get_card and put_card mb final

--- scripts/Deck.gd
+++ scripts/Deck.gd
@@ -1,18 +1,15 @@
-extends Node
-var deck = []
-var hend = []
+class DeckClass:
+	var deck = []
 
-func _init(deck):
-	self.deck = deck.duplicate(true)
+	func _init(deck):
+		self.deck = deck.duplicate(true)
 
-func get_card():
-	randomize()
-	self.deck.shuffle()
-	hend.append(self.deck[0])
-	self.deck.remove(0)
-	return hend
+	func get_card():
+		randomize()
+		self.deck.shuffle()
+		if(deck.size() == 0):
+			return null
+		return deck.pop_back()
 
-func put_card():
-	for i in hend.size():
-		self.deck.append(hend[i])
-	hend.clear()
+	func put_card(card):
+		self.deck.append(card)
